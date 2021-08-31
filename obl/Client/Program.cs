a5b065c97093fcf93176dfe389e247656ca8538d
Nicolas <alannicolasort@gmail.com>
2021-08-31 17:07:50 -0300

    agrego cliente y servidor de prueba

--- obl/Client/Program.cs
+++ obl/Client/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-namespace Client
+namespace ConsoleAppSocketClient
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        } 
+            Console.WriteLine("Empezando Socket Client...");
+
+            var socketClient = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            //127.0.0.1 es localhost -> solo permite conexiones dentro de la misma maquina
+            // RANGO DE PUERTOS 0 - 65535 (RANGO de 1 a 1024 es reservado)
+            var remoteEndpoint = new IPEndPoint(IPAddress.Parse("192.168.1.5"),30000);
+            socketClient.Connect(remoteEndpoint);
+            
+            Console.WriteLine("Conectado al server remoto, escriba un mensaje, enter para terminar");
+
+            var termine = false;
+            while (!termine)
+            {
+                var message = Console.ReadLine();
+                if (message.Length == 0)
+                {
+                    Console.WriteLine("Cerrando la conexion");
+                    termine = true;
+                }
+                else
+                {
+                    var data = Encoding.UTF8.GetBytes(message);
+                    socketClient.Send(data);
+                }
+            }
+            socketClient.Shutdown(SocketShutdown.Both);
+            socketClient.Close();
+            Console.WriteLine("Cerrando la conexion..");            
+            Console.ReadLine();
+        }
     }
 }
--- obl/Server/Program.cs
+++ obl/Server/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
-namespace Server
+namespace ConsoleAppSocketServer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Comenzando Socket Server...");
+
+            var socketServer = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+            Console.WriteLine("Creado el Socket");
+            //127.0.0.1 es localhost -> solo permite conexiones dentro de la misma maquina
+            var localEndpoint = new IPEndPoint(IPAddress.Parse("192.168.1.5"),30000);
+            
+            socketServer.Bind(localEndpoint);
+            Console.WriteLine("Socket bound to local IP");
+            //Luego del listen, ponemos el socket server en modo de aceptar conexiones y ya NO lo 
+            // podemos usar para recibir o enviar datos
+            socketServer.Listen(10);
+            Console.WriteLine("Socket esta en modo escucha");
+            var Termine = false;
+            var threadCount = 0;
+            while (!Termine)
+            {
+                var connectedSocket = socketServer.Accept();
+                threadCount++;
+                var threadId = threadCount;
+                Console.WriteLine("Acepte una conexion...");
+                var threadConnection = new Thread(() => HandleConnection(connectedSocket,threadId));
+                threadConnection.Start();
+            }
+        }
+
+        private static void HandleConnection(Socket connectedSocket, int threadId)
+        {
+            var bytesReceived = 1;
+            // Este while se usa para mantenerse aqui mientras la conexion no se cierra
+            while (bytesReceived > 0)
+            {
+                var buffer = new byte[1024];
+                // Si la conexion se cierra, el receive retorna 0
+                bytesReceived = connectedSocket.Receive(buffer);
+                if (bytesReceived > 0)
+                {
+                    var message = Encoding.UTF8.GetString(buffer);
+                    Console.WriteLine($"{threadId}: Recibi el siguiente mensaje: {message}");
+                }
+                else
+                {
+                    Console.WriteLine($"{threadId}: El cliente remoto cerro la conexion...");
+                }
+            }
+
+            // Iniciamos el proceso de cerrado del socket
+            connectedSocket.Shutdown(SocketShutdown.Both);
+            connectedSocket.Close();
+            Console.WriteLine($"{threadId}: Cerrando la  conexion...");
         }
     }
 }
