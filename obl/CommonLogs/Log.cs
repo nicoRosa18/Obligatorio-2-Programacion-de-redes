e9ec5ae07f9b630483d599f5bbb4eedb39ea7f4e
Gaston Landeira <gastonlandeira@gmail.com>
2021-11-02 18:23:56 -0300

    Created projects, references and installed rabbit package. Also created common object

---
+++ obl/CommonLogs/Log.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CommonLogs
+{
+    public class Log
+    {
+        public string User {get; set;}
+        public string EventType {get; set;}
+        public DateTime Time {get; set;}
+        public string EventDescription {get; set;}
+    }
+}
---
+++ obl/ServerLogs/Program.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+
+namespace ServerLogs
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+        }
+    }
+}
