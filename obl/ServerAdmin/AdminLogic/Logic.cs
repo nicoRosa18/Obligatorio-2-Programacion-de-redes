db5b8c6f66a7b2e9ab0a662f412a48a24f9abdb1
Nicolas <alannicolasort@gmail.com>
2021-11-21 17:30:00 -0300

    methods ond logic, usercontroller, gamecontroller and Grpcmanager

--- obl/ServerAdmin/AdminLogic/Logic.cs
+++ obl/ServerAdmin/AdminLogic/Logic.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using ServerAdmin.DTOs;
 using ServerAdmin.Exceptions;
 using ServerAdmin.ServerCommunication;
 
@@ -14,10 +15,83 @@ namespace ServerAdmin.AdminLogic
             _communication = new GrpcManager();
         }
 
+        public Task GetUser(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetGame(string id)
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task AddUserAsync(string userName)
         {
             Reply possibleError = await _communication.AddUserAsync(userName);
-            if(possibleError.Error)
+            if (possibleError.Error)
+            {
+                throw new UserException(possibleError.ErrorDescription);
+            }
+        }
+
+        public async Task ModifyUserAsync(string oldName, string newName)
+        {
+            Reply possibleError = await _communication.ModifyUserAsync(oldName, newName);
+            if (possibleError.Error)
+            {
+                throw new UserException(possibleError.ErrorDescription);
+            }
+        }
+
+        public async Task RemoveUserAsync(string userName)
+        {
+            Reply possibleError = await _communication.RemoveUserAsync(userName);
+            if (possibleError.Error)
+            {
+                throw new UserException(possibleError.ErrorDescription);
+            }
+        }
+
+        public async Task AddGameAsync(GameDTO game)
+        {
+            Reply possibleError = await _communication.AddGameAsync(game);
+            if (possibleError.Error)
+            {
+                throw new UserException(possibleError.ErrorDescription);
+            }
+        }
+
+        public async Task ModifyGameAsync(string oldGameTitle, GameDTO game)
+        {
+            Reply possibleError = await _communication.ModifyGameAsync(oldGameTitle, game);
+            if (possibleError.Error)
+            {
+                throw new UserException(possibleError.ErrorDescription);
+            }
+        }
+
+        public async Task RemoveGameAsync(string gameTitle)
+        {
+            Reply possibleError = await _communication.RemoveGameAsync(gameTitle);
+            if (possibleError.Error)
+            {
+                throw new UserException(possibleError.ErrorDescription);
+            }
+        }
+
+        public async Task AssociateGameAsync(string gameTitle, string userName)
+        {
+            Reply possibleError = await _communication.AsociateGameAsync(gameTitle, userName);
+            if (possibleError.Error)
+            {
+                throw new UserException(possibleError.ErrorDescription);
+            }
+        }
+
+        public async Task DisassociateGameAsync(string gameTitle, string userName)
+        {
+            Reply possibleError = await _communication.DisassociateGameAsync(gameTitle, userName);
+            if (possibleError.Error)
             {
                 throw new UserException(possibleError.ErrorDescription);
             }
--- obl/ServerAdmin/ServerCommunication/IGrpcManager.cs
+++ obl/ServerAdmin/ServerCommunication/IGrpcManager.cs
@@ -1,10 +1,18 @@
 using System;
 using System.Threading.Tasks;
+using ServerAdmin.DTOs;
 
 namespace ServerAdmin.ServerCommunication
 {
     public interface IGrpcManager
     {
         Task<Reply> AddUserAsync(string userName);
+        Task<Reply> ModifyUserAsync(string oldName, string newName);
+        Task<Reply> RemoveUserAsync(string userName);
+        Task<Reply> AddGameAsync(GameDTO game);
+        Task<Reply> AsociateGameAsync(string gameTitle, string userName);
+        Task<Reply> ModifyGameAsync(string oldGameTitle, GameDTO game);
+        Task<Reply> RemoveGameAsync(string gameTitle);
+        Task<Reply> DisassociateGameAsync(string gameTitle, string userName);
     }
 }
